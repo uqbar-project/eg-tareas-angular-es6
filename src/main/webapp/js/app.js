dc4bbe8ddce5690f9bdc8e641c55f03d9d31ddb8
fdodino <fernando.dodino@gmail.com>
2017-06-26 17:42:44 -0300

    Initial fresh version

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,7 +1,4 @@
 angular
     .module('tareasApp', ['ui.router'])
-    .service('usuariosService', UsuarioService)
-    .service('tareaService', TareaService)
-    .component('asignarTarea', asignarTarea)
-    .controller('tareasController', TareasController)
+    .controller('tareasController', TareaController)
     .config(routes)
--- src/main/webapp/js/components/errorHandler.js
+++
@@ -1,8 +0,0 @@
-function notificarError(controller, mensaje) {
-	controller.errors.push(mensaje.data)
-	controller.$timeout(() => {
-		while (controller.errors.length > 0)
-			controller.errors.pop()
-	}, 3000)
-}
-
